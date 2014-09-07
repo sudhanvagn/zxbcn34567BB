392ffdc2e77dd530853887e12565ae96fd62380d
Sudhanva Gnaneshwar <sudhanva.gn@gmail.com>
2014-09-07 12:28:25 -0700

    Transistion to new slides changes

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -21,6 +21,20 @@ function(Backbone,SlidesView,SlidesCollections, MainRouter){
 			
 			App.router = new MainRouter();
 			Backbone.history.start();			
+		},
+
+		events:{
+			'keyup': 'keyUp'
+		},
+
+		keyUp: function(e){
+			//37 = left
+			//39 = right
+			if(e.keyCode === 37 || e.keyCode === 39){
+				App.Vent.trigger ('changeSlide',{
+					direction: e.keyCode === 39 ? 'next' : 'prev'
+				});
+			}
 		}
 	});
 
--- app/scripts/views/slides.js
+++ app/scripts/views/slides.js
@@ -3,7 +3,13 @@ define(['backbone','views/slide'], function(Backbone,SlideView){
 		el: $('.slides'),	
 
 		initialize: function(){
+			this.currentSlideIndex = 1;
+			this.numSlides = this.collection.length;
+			this.transitionSpeed = 600;
 			this.renderAll();
+
+			App.Vent.on('init', this.hideAllButFirst,this);
+			App.Vent.on('changeSlide',this.changeSlide, this);
 		},
 
 		renderAll: function(){
@@ -15,6 +21,66 @@ define(['backbone','views/slide'], function(Backbone,SlideView){
 			var slideView = new SlideView({model: slide});
 			this.$el.append(slideView.render().el);
 			return this;
+		},
+
+		hideAllButFirst: function(){
+			this.$el.children(':nth-child(n+2)').hide();
+		},
+
+		changeSlide: function(opts){
+			var self = this;
+			var slides = this.$el.children();
+			var newSlide;
+
+			this.setCurrentSlideIndex(opts)
+			
+			newSlide = this.getNextSlide(slides);
+
+			this.animateToNewSlide(slides, newSlide, opts.direction);
+			
+			App.router.navigate('/slides'+this.currentSlideIndex);
+		},
+
+		getNextSlide : function(slides){
+			return slides.eq(this.currentSlideIndex - 1);
+		},
+		animateToNewSlide: function(slides, newSlide, direction){
+			slides.filter(':visible')
+				.css('position','absolute') //Temporary
+				.animate({
+					top: direction === 'next'? '100%' : '-100%',
+					opacity: 'hide'
+				}, this.transitionSpeed, function(){
+					// triggered when file is gone from the screen
+					$(this).css('top',0);
+
+					//bring new slide into view
+					newSlide
+						.css('position','absolute')
+						.css('top',direction === 'next' ? '-100%' : '100%')
+						.animate({
+							top: 0,
+							opacity: 'show'},
+							self.transitionSpeed);
+					});
+		},
+		setCurrentSlideIndex: function(opts){
+			//If we're requesting a special slide
+			// then set current index
+			if(opts.slideIndex){
+				return this.currentSlideIndex = ~~opts.slideIndex;
+			}
+
+			this.currentSlideIndex += opts.direction === 'next' ? 1 : -1;
+
+			if(this.currentSlideIndex > this.numSlides){
+				this.currentSlideIndex = 1;
+			}
+
+			if(this.currentSlideIndex <= 0){
+				this.currentSlideIndex = this.numSlides;
+			}
+
 		}
 	});
 
